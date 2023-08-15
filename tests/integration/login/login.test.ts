2c9eb862d8a2f8dd95181df29b1169383ca0a347
fabiotorres95 <engfabiotorres@outlook.com>
2023-08-15 12:02:28 -0300

    created first test for POST /login

--- tests/integration/login/login.test.ts
+++ tests/integration/login/login.test.ts
@@ -1,10 +1,21 @@
 import sinon from 'sinon';
 import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
+import loginMock from '../../mocks/login.mock';
+
+import app from '../../../src/app'
 
 chai.use(chaiHttp);
 
 describe('POST /login', function () { 
   beforeEach(function () { sinon.restore(); });
 
+  it('Retorna um erro ao não passar o nome de usuário', async function () {
+    const body = loginMock.loginBodyNoUser;
+
+    const response = await chai.request(app).post('/login').send(body);
+
+    expect(response.status).to.equal(400);
+    expect(response.body).to.be.deep.equal({ message: '"username" and "password" are required' })
+  })
 });
---
+++ tests/mocks/login.mock.ts
@@ -0,0 +1,6 @@
+const goodPassword = '$2a$10$i6MktJDbpj01fisUaPSW3OuVdZy01ID7SjxzAKfogvhOE2QwomO.C';
+const loginBodyNoUser = { username: undefined, password: goodPassword };
+
+export default {
+  loginBodyNoUser,
+}
