84c4146f37169ff427c8bd076d5bcc7209e7b888
fabiotorres95 <engfabiotorres@outlook.com>
2023-08-15 13:36:01 -0300

    created tests for POST /login validations

--- tests/integration/login/login.test.ts
+++ tests/integration/login/login.test.ts
@@ -4,6 +4,7 @@ import chaiHttp from 'chai-http';
 import loginMock from '../../mocks/login.mock';
 
 import app from '../../../src/app'
+import UserModel from '../../../src/database/models/user.model';
 
 chai.use(chaiHttp);
 
@@ -17,5 +18,35 @@ describe('POST /login', function () {
 
     expect(response.status).to.equal(400);
     expect(response.body).to.be.deep.equal({ message: '"username" and "password" are required' })
+  })
+
+  it('Retorna um erro ao não passar uma senha', async function () {
+    const body = loginMock.loginBodyNoPassword;
+
+    const response = await chai.request(app).post('/login').send(body);
+
+    expect(response.status).to.equal(400);
+    expect(response.body).to.be.deep.equal({ message: '"username" and "password" are required' });
+  })
+
+  it('Retorna um erro se o nome de usuário está errado', async function () {
+    const body = loginMock.loginBodyBadUser;
+    sinon.stub(UserModel, 'findAll').resolves([]);
+
+    const response = await chai.request(app).post('/login').send(body);
+
+    expect(response.status).to.equal(401);
+    expect(response.body).to.be.deep.equal({ message: 'Username or password invalid' });
+  })
+
+  it('Retorna um erro se a senha está errada', async function () {
+    const body = loginMock.loginBodyBadPassword;
+    const mockGoodResponse = UserModel.build(loginMock.goodUser);
+    sinon.stub(UserModel, 'findAll').resolves([mockGoodResponse])
+
+    const response = await chai.request(app).post('/login').send(body);
+
+    expect(response.status).to.equal(401);
+    expect(response.body).to.be.deep.equal({ message: 'Username or password invalid' });
   })
 });
--- tests/mocks/login.mock.ts
+++ tests/mocks/login.mock.ts
@@ -1,6 +1,25 @@
+const goodUsername = 'Hagar';
 const goodPassword = '$2a$10$i6MktJDbpj01fisUaPSW3OuVdZy01ID7SjxzAKfogvhOE2QwomO.C';
+const badUsername = 'Asterix';
+const badPassword = 'XablauXablauXablauXablauXablau';
+
+const goodUser = {
+  id: 1,
+  username: goodUsername,
+  vocation: 'Guerreiro',
+  level: 10,
+  password: goodPassword,
+};
+
 const loginBodyNoUser = { username: undefined, password: goodPassword };
+const loginBodyNoPassword = { username: goodUsername, password: undefined };
+const loginBodyBadUser = { username: badUsername, password: goodPassword };
+const loginBodyBadPassword = { username: goodUsername, password: badPassword};
 
 export default {
   loginBodyNoUser,
+  loginBodyNoPassword,
+  loginBodyBadUser,
+  loginBodyBadPassword,
+  goodUser,
 }
