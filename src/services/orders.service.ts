2c5a1f81cce1f1f7d6b1092f6d04c404096c2892
fabiotorres95 <engfabiotorres@outlook.com>
2023-08-14 22:32:40 -0300

    GET /orders gives the right results

--- src/services/orders.service.ts
+++ src/services/orders.service.ts
@@ -1,9 +1,25 @@
-import OrderModel, { OrderSequelizeModel } from '../database/models/order.model';
+import OrderModel from '../database/models/order.model';
+import ProductModel from '../database/models/product.model';
+import { Order } from '../types/Order';
 
-async function getAllOrders() : Promise<OrderSequelizeModel[]> {
+async function getAllOrders() : Promise<Order[]> {
   const allOrders = await OrderModel.findAll();
+  const allProducts = await ProductModel.findAll();
 
-  return allOrders;
+  const newOrders = allOrders.map((order) => {
+    const newOrder = order.dataValues;
+    newOrder.productIds = [];
+    return newOrder;
+  });
+
+  allProducts.map((prod) => { 
+    const { id, orderId } = prod.dataValues;
+    const idArray = newOrders[orderId - 1].productIds as number[];
+    idArray.push(id);
+    return null;
+  });
+
+  return newOrders;
 }
 
 export default {
