479f5afacbd18bba2b9f8587523c19b9cd85159a
fabiotorres95 <engfabiotorres@outlook.com>
2023-08-15 14:17:22 -0300

    created test for GET /products

--- tests/integration/products/list.test.ts
+++ tests/integration/products/list.test.ts
@@ -1,10 +1,28 @@
 import sinon from 'sinon';
 import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
+import ProductModel from '../../../src/database/models/product.model';
+import productsMock from '../../mocks/products.mock';
+import app from '../../../src/app';
 
 chai.use(chaiHttp);
 
 describe('GET /products', function () { 
   beforeEach(function () { sinon.restore(); });
 
+  it('Retorna todos os produtos cadastrados com sucesso', async function () {
+    const product1DB = ProductModel.build(productsMock.product1);
+    const product2DB = ProductModel.build(productsMock.product2);
+    const product3DB = ProductModel.build(productsMock.product3);
+    sinon.stub(ProductModel, 'findAll').resolves([
+      product1DB,
+      product2DB,
+      product3DB,
+    ]);
+
+    const response = await chai.request(app).get('/products');
+
+    expect (response.status).to.equal(200);
+    expect (response.body).to.deep.equal(productsMock.allProducts);
+  })
 });
---
+++ tests/mocks/products.mock.ts
@@ -0,0 +1,31 @@
+const product1 = {
+  id: 1,
+  name: 'Pedra Filosofal',
+  price: '20 gold',
+  orderId: 2
+};
+const product2 = {
+  id: 2,
+  name: 'Lança do Destino',
+  price: '100 diamond',
+  orderId: 1
+};
+const product3 = {
+  id: 3,
+  name: 'Bombas do Morshu',
+  price: '...enough Rupees',
+  orderId: 3
+};
+
+const allProducts = [
+  product1,
+  product2,
+  product3
+];
+
+export default {
+  product1,
+  product2,
+  product3,
+  allProducts,
+}
